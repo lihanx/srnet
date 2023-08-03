89c0d2316b82e2a221b7df635a601ee74b8c71a6
lihanx <lihanx9@163.com>
2023-08-03 13:28:14 +0800

    修改转置卷积 kernel_size

--- src/models/blocks.py
+++ src/models/blocks.py
@@ -3,15 +3,10 @@
 from typing import Optional, Callable
 
 from torch import Tensor, nn
-from torchvision.models.resnet import conv1x1, conv3x3
+from torchvision.models.resnet import conv1x1
 
 
 class TransposeBottleneck(nn.Module):
-    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
-    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
-    # according to "Deep residual learning for image recognition" https://arxiv.org/abs/1512.03385.
-    # This variant is also known as ResNet V1.5 and improves accuracy according to
-    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
 
     contraction: float = 0.5
 
@@ -32,7 +27,7 @@ class TransposeBottleneck(nn.Module):
         self.conv1 = conv1x1(inplanes, width)
         self.bn1 = norm_layer(width)
         if upsample:
-            self.conv2 = nn.ConvTranspose2d(width, width, kernel_size=4, stride=stride, padding=1, bias=False)
+            self.conv2 = nn.ConvTranspose2d(width, width, kernel_size=2, stride=stride, bias=False)
         else:
             self.conv2 = nn.ConvTranspose2d(width, width, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = norm_layer(width)
@@ -59,7 +54,7 @@ class TransposeBottleneck(nn.Module):
         if self.upsample is not None:
             identity = self.upsample(x)
             print(identity.shape)
-        # todo 处理转置卷积维度不一致的问题
+
         out += identity
         out = self.relu(out)
 
