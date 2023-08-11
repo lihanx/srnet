a998edee90ed3a2acb8a083b20d399f570b66658
lihanx <lihanx9@163.com>
2023-08-11 21:53:23 +0800

    完成初版训练代码

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@
 import datetime
 import os.path
 import logging
-import time
+logging.basicConfig(level=logging.INFO)
 
 import torch
 from torch.utils.data import DataLoader
@@ -13,6 +13,7 @@ from torch.utils.tensorboard import SummaryWriter
 from .dataset import RandAugmentationDataSet
 from .models import SRNet
 from .loss import SSIMLoss
+from .utils import _check_dir
 
 
 logger = logging.getLogger(__name__)
@@ -26,7 +27,11 @@ class SRNetTrainer:
         self.batch_size = 64
         cwd = os.path.abspath(os.path.dirname(__file__))
         self.checkpoint_path = os.path.join(cwd, "checkpoints")
+        _check_dir(self.checkpoint_path)
         self.summary_path = os.path.join(cwd, "runs")
+        _check_dir(self.summary_path)
+        self.weight_path = os.path.join(cwd, "weights")
+        _check_dir(self.weight_path)
         self.dataset = RandAugmentationDataSet(path=os.path.join(cwd, "images"), origin_dir="origin", reduced_dir="reduced", limit=640000)
         self.train_dataloader = DataLoader(self.dataset, batch_size=self.batch_size)
         self.test_dataloader = DataLoader(self.dataset, batch_size=self.batch_size)
@@ -35,11 +40,11 @@ class SRNetTrainer:
         self.net.to(self.device)
         self.optimizer = Adam(self.net.parameters(), lr=self.learning_rate)
         self.loss_fn = SSIMLoss()
-
         self.lr_decay_rate = 0.8
         self.lr_epoch_per_decay = 100
         self.scheduler = LambdaLR(optimizer=self.optimizer, lr_lambda=lambda epoch: self.lr_decay_rate ** (epoch // self.lr_epoch_per_decay))
-        self.tensorboard_writer = SummaryWriter(os.path.join(self.summary_path, f"srnet_trainer_{datetime.datetime.now():%Y%m%d%_H%M%S}"))
+        self._training_date = datetime.datetime.now()
+        self.summary_writer = SummaryWriter(os.path.join(self.summary_path, f"srnet_trainer_{self._training_date:%Y%m%d%_H%M%S}"))
 
     def save_checkpoints(self, epoch, loss_val) -> None:
         checkpoint = {
@@ -64,8 +69,10 @@ class SRNetTrainer:
                 last_checkpoint = filepath
         return torch.load(last_checkpoint)
 
-    def _train(self):
+    def _train(self, epoch):
         self.net.train()
+        running_loss = 0.
+        last_loss = 0.
         for batch_idx, (x, y) in enumerate(self.train_dataloader):
             self.optimizer.zero_grad()  # To reset the gradients of model parameters. Gradients by default add up; to prevent double-counting, we explicitly zero them at each iteration.
             x, y = x.to(self.device), y.to(self.device)
@@ -75,30 +82,50 @@ class SRNetTrainer:
             loss.backward()
             self.optimizer.step()  # To adjust the parameters by the gradients collected in the backward pass.
 
+            running_loss += loss.item()
             if batch_idx % 10 == 0:
-                train_loss = loss.item()
+                last_loss = running_loss / 10
+                logger.info(f"Train batch {batch_idx + 1} loss: {last_loss}")
+                tb_x = epoch * len(self.train_dataloader) + batch_idx + 1
+                self.summary_writer.add_scalar("Loss/train", last_loss, tb_x)
+                running_loss = 0.
+        return last_loss
 
-    def _test(self):
+    def _test(self, epoch):
         self.net.eval()
-
-        test_loss, correct = 0, 0
-        num_batches = len(self.test_dataloader)
-        size = self.dataset.limit
-
+        test_loss = 0.
+        last_loss = 0.
         with torch.no_grad():
-            for x, y in self.test_dataloader:
+            for batch_idx, (x, y) in enumerate(self.test_dataloader):
                 pred = self.net(x)
                 test_loss += self.loss_fn(pred, y).item()
-                correct = 1 - test_loss
-
-        test_loss /= num_batches
-        correct /= size
-        logger.info(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+                if batch_idx % 10 == 0:
+                    last_loss = test_loss / 10
+                    logger.info(f"Validate batch {batch_idx + 1} loss: {last_loss}")
+                    tb_x = epoch * len(self.test_dataloader) + batch_idx + 1
+                    self.summary_writer.add_scalar("Loss/validate", last_loss, tb_x)
+                    test_loss = 0.
+        return last_loss
 
     def train(self):
+
+        limit = 0.97
+        best_ssim = 0.
         for epoch in range(self.epochs):
-            self._train()
-            self._test()
+            tloss = self._train(epoch)
+            vloss = self._test(epoch)
+            self.summary_writer.add_scalars(
+                "Training vs. Validation Loss",
+                {"Training": tloss, "Validation": vloss},
+                epoch+1
+            )
             self.scheduler.step()
+            if 1 - vloss >= 0.97:
+                logger.info(f"SSIM >= {limit}, Stop Training.")
+                best_ssim = 1 - vloss
+                break
+        torch.save(self.net, f"srnet_{self._training_date:%Y%m%d%H%M%S}_loss{best_ssim}_.pth")
+        logger.info("Model saved.")
+        logger.info("Done.")
 
 
